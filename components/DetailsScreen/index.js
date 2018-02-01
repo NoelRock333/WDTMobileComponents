e6c94476ca21ebc074fe4b4e54152cb43d5ecb71
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-01 01:05:38 +0000

    Details screen style

--- components/DetailsScreen/index.js
+++ components/DetailsScreen/index.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text, Button } from 'native-base';
+import { Text, Button, Content, Container, Header, Body, Title, Left, Right, Icon } from 'native-base';
 
 const DetailsScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#22B188' }}>
-    <Text style={{ color: 'white' }}>Details Screen</Text>
-    <View style={{ justifyContent: 'center' }}>
-      <Button danger onPress={() => navigation.goBack()}>
-        <Text>Go to back</Text>
-      </Button>
-      <Button warning onPress={() => navigation.navigate('GridScreenCols')}>
-        <Text>Grid Screen Cols</Text>
-      </Button>
-      <Button warning onPress={() => navigation.navigate('GridScreenRows')}>
-        <Text>Grid Screen Rows</Text>
-      </Button>
-      <Button warning onPress={() => navigation.navigate('VectorIcons')}>
-        <Text>Vector Icons</Text>
-      </Button>
-      <Button warning onPress={() => navigation.navigate('Swipable')}>
-        <Text>Swipable</Text>
-      </Button>
-    </View>
-  </View>
+  <Container>
+    <Header>
+      <Left style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Icon name="arrow-back"></Icon>
+        <Text style={{ marginLeft: 10 }}>Back</Text>
+      </Left>
+      <Body>
+        <Title>Details Screen</Title>
+      </Body>
+      <Right />
+    </Header>
+    <Content style={{ backgroundColor: '#22B188' }}>
+      <View style={{ justifyContent: 'center' }}>
+        <Button danger block onPress={() => navigation.goBack()}>
+          <Text>Go to back</Text>
+        </Button>
+        <Button warning block onPress={() => navigation.navigate('GridScreenCols')}>
+          <Text>Grid Screen Cols</Text>
+        </Button>
+        <Button warning block onPress={() => navigation.navigate('GridScreenRows')}>
+          <Text>Grid Screen Rows</Text>
+        </Button>
+        <Button warning block onPress={() => navigation.navigate('VectorIcons')}>
+          <Text>Vector Icons</Text>
+        </Button>
+        <Button warning block onPress={() => navigation.navigate('Swipable')}>
+          <Text>Swipable</Text>
+        </Button>
+      </View>
+    </Content>
+  </Container>
 );
 
 export default DetailsScreen;
